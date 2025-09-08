291baa7f0d71159068df0daab6eebc6b61f4e700
cohearne77 <82214194+cohearne77@users.noreply.github.com>
2025-09-08 12:18:49 +0100

    Add files via upload

---
+++ prismasase_settings - sample.py
@@ -0,0 +1,6 @@
+######################################################
+# Service Account
+######################################################
+PRISMASASE_CLIENT_ID="ID"
+PRISMASASE_CLIENT_SECRET="secret"
+PRISMASASE_TSG_ID="tsg"
